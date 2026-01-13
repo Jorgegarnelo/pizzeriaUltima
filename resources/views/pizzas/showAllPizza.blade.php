d22b5f88c07ebde63fa483794261c53c05cfa6ca
Jorge Garnelo Lopez <jorgegarnelolopez@gmail.com>
2026-01-13 11:55:47 +0100

    segunda vista

--- resources/views/pizzas/showAllPizza.blade.php
+++ resources/views/pizzas/showAllPizza.blade.php
@@ -1,12 +1,12 @@
 <table>
     <tr>
-        <th></th>
-        <th></th>
+        <th>Pizza</th>
+        <th>Precio</th>
     </tr>
 @foreach ($pizzas as $pizza)
     <tr>
-        <td>{{$pizza->nombre}}</td>
-        <td></td>
+        <td><a href="{{route('pizzas.showOnePizza', $pizza->id) }}"></a>{{$pizza->nombre}}</td>
+        <td>{{$pizza->precio}}</td>
     </tr>
 @endforeach    
 </table>
